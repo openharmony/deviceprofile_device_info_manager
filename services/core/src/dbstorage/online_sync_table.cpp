6b119986a53dd4cf3284e7c6bb555bfe909bbc07
Sven Wang <wanghancai@huawei.com>
2022-10-18 15:05:25 +0800

    add device matrix
    Signed-off-by: Sven Wang <wanghancai@huawei.com>

--- services/core/src/dbstorage/online_sync_table.cpp
+++ services/core/src/dbstorage/online_sync_table.cpp
@@ -44,11 +44,13 @@ void OnlineSyncTable::Init()
     Options options = {
         .createIfMissing = true,
         .encrypt = true,
-        .autoSync = false,
+        .autoSync = true,
         .kvStoreType = KvStoreType::SINGLE_VERSION,
         .area = 1,
         .baseDir = "/data/service/el1/public/database/distributed_device_profile_service"
     };
+	// clean the IMMEDIATE_SYNC_ON_CHANGE
+	options.policies = {};
     SyncPolicy syncPolicy {
         .type = PolicyType::IMMEDIATE_SYNC_ON_ONLINE
     };
