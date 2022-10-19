f560d4410d8d225ffa1b27f4eec88167de3c6426
zuojiangjiang <zuojiangjiang@huawei.com>
2022-10-19 10:07:24 +0800

    fix error Signed-off-by: zuojiangjiang <zuojiangjiang@huawei.com>

--- services/core/src/dbstorage/online_sync_table.cpp
+++ services/core/src/dbstorage/online_sync_table.cpp
@@ -49,7 +49,7 @@ void OnlineSyncTable::Init()
         .area = 1,
         .baseDir = "/data/service/el1/public/database/distributed_device_profile_service"
     };
-	// clean the IMMEDIATE_SYNC_ON_CHANGE
+    // clean the IMMEDIATE_SYNC_ON_CHANGE
     options.policies = {};
     SyncPolicy syncPolicy {
         .type = PolicyType::IMMEDIATE_SYNC_ON_ONLINE
