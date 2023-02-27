f08ce72a83ad7a9b640d526084b524387ab7f60e
zhangmingxiang <zhangmingxiang@huawei.com>
2023-02-27 12:04:08 +0000

    access to dynamic start-stop framework
    Signed-off-by: zhangmingxiang <zhangmingxiang@huawei.com>
    Change-Id: I98d48f0ea5e6315120cf7e401d7698f271b40b0f
    Signed-off-by: zhangmingxiang <zhangmingxiang@huawei.com>

--- services/core/src/contentsensor/app_info_collector.cpp
+++ services/core/src/contentsensor/app_info_collector.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -16,7 +16,7 @@
 #include "app_info_collector.h"
 
 #include "device_profile_log.h"
-#include "distributed_ability_manager_interface.h"
+#include "dms_constant.h"
 #include "nlohmann/json.hpp"
 
 namespace OHOS {
@@ -46,8 +46,8 @@ bool AppInfoCollector::ConvertToProfileData(ServiceCharacteristicProfile& profil
 
 bool AppInfoCollector::GetDmsVersionInfo(std::string& dmsName, std::string& dmsVersionData)
 {
-    dmsName = DistributedSchedule::DMS_NAME;
-    dmsVersionData = DistributedSchedule::DMS_VERSION;
+    dmsName = DistributedSchedule::Constants::DMS_NAME;
+    dmsVersionData = DistributedSchedule::Constants::DMS_VERSION;
     return true;
 }
 } // namespace DeviceProfile
