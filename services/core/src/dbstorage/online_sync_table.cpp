f41f2b91ee000bdb795167487a417e57da4b5d14
wangbaidong <wangbaidong@huawei.com>
2023-10-20 10:27:42 +0800

    1020
    Signed-off-by: wangbaidong <wangbaidong@huawei.com>

--- services/core/src/dbstorage/online_sync_table.cpp
+++ services/core/src/dbstorage/online_sync_table.cpp
@@ -46,6 +46,7 @@ void OnlineSyncTable::Init()
         .encrypt = true,
         .autoSync = true,
         .kvStoreType = KvStoreType::SINGLE_VERSION,
+        .securityLevel = DistributedKv::SecurityLevel::S2,
         .area = 1,
         .baseDir = "/data/service/el1/public/database/distributed_device_profile_service"
     };
