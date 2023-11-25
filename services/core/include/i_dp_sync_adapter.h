40330f3c40e23ee055d5de1551dc9aa00fdfe65f
torrizo <lishenhao2@huawei.com>
2023-11-25 07:45:49 +0000

    update services/core/include/i_dp_sync_adapter.h.
    Signed-off-by: torrizo <lishenhao2@huawei.com>

--- services/core/include/i_dp_sync_adapter.h
+++ services/core/include/i_dp_sync_adapter.h
@@ -30,8 +30,9 @@ public:
     virtual ~IDPSyncAdapter() = default;
     virtual int32_t Initialize() = 0;
     virtual int32_t Release() = 0;
+    virtual int32_t DetectRemoteDPVersion(const std::string& peerDevId) = 0;
     virtual int32_t SyncProfile(const std::list<std::string>& deviceIdList,
-        const sptr<ISyncCompletedCallback>& syncCb) = 0;
+        const sptr<IRemoteObject>& syncCompletedCallback) = 0;
 };
 using CreateDPSyncAdapterFuncPtr = IDPSyncAdapter *(*)(void);
 } // namespace DeviceProfile
