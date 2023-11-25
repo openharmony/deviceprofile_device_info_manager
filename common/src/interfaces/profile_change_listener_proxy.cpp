50f5da8f1defa0bd7d12e6beeac8b40b0138c123
tangfan <tangfan5@huawei.com>
2023-11-25 15:39:16 +0800

    fix sync cache bug4
    Signed-off-by: tangfan <tangfan5@huawei.com>

--- common/src/interfaces/profile_change_listener_proxy.cpp
+++ common/src/interfaces/profile_change_listener_proxy.cpp
@@ -85,7 +85,7 @@ int32_t ProfileListenerProxy::OnDeviceProfileAdd(const DeviceProfile& profile)
         return ERR_FLATTEN_OBJECT;
     }
     MessageParcel reply;
-    SEND_REQUEST(remote, static_cast<uint32_t>(DPInterfaceCode::ON_TRUST_DEVICE_PROFILE_ADD), data, reply);
+    SEND_REQUEST(remote, static_cast<uint32_t>(DPInterfaceCode::ON_DEVICE_PROFILE_ADD), data, reply);
     return DP_SUCCESS;
 }
 
