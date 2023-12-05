a11d05b0e182ba79e9bd7dd1579ddeefb8d5e8ba
zhanglei <zhanglei730@huawei.com>
2023-12-05 14:56:33 +0800

    add UT
    Signed-off-by: zhanglei <zhanglei730@huawei.com>

---
+++ services/core/test/unittest/permission_manager_test.cpp
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include "profile_utils.h"
+#include "permission_manager.h"
+#include "distributed_device_profile_constants.h"
+#include "distributed_device_profile_log.h"
+#include "distributed_device_profile_errors.h"
+
+
+using namespace testing::ext;
+using namespace OHOS::DistributedDeviceProfile;
+using namespace std;
+namespace {
+    const std::string TAG = "PermissionManagerTest";
+}
+class PermissionManagerTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp();
+    void TearDown();
+};
+
+void PermissionManagerTest::SetUpTestCase()
+{
+    int32_t ret = PermissionManager::GetInstance().Init();
+    EXPECT_EQ(ret, DP_SUCCESS);
+}
+
+void PermissionManagerTest::TearDownTestCase()
+{
+    int32_t ret = PermissionManager::GetInstance().UnInit();
+    EXPECT_EQ(ret, DP_SUCCESS);
+}
+
+void PermissionManagerTest::SetUp()
+{
+}
+
+void PermissionManagerTest::TearDown()
+{
+}
+
+/*
+ * @tc.name: IsCallerTrust_001
+ * @tc.desc: Normal testCase of PermissionManagerTest for CRUD
+ * @tc.type: FUNC
+ */
+HWTEST_F(PermissionManagerTest, IsCallerTrust_001, TestSize.Level1)
+{
+    bool ret = PermissionManager::GetInstance().IsCallerTrust(PUT_ACCESS_CONTROL_PROFILE);
+    EXPECT_EQ(ret, false);
+}
