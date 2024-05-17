a9a0311665bea0bc09b5eb90203564046fc03ecf
chenshuo <chenshuo42@huawei.com>
2024-05-18 02:47:15 +0800

    数据分级预制
    Signed-off-by: chenshuo <chenshuo42@huawei.com>

---
+++ services/core/include/deviceprofilemanager/static_profile_manager.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_DP_STATIC_PROFILE_MANAGER_H
+#define OHOS_DP_STATIC_PROFILE_MANAGER_H
+
+#include "characteristic_profile.h"
+#include "kv_adapter.h"
+#include "single_instance.h"
+
+namespace OHOS {
+namespace DistributedDeviceProfile {
+class StaticProfileManager {
+DECLARE_SINGLE_INSTANCE(StaticProfileManager);
+
+public:
+    int32_t Init();
+    int32_t UnInit();
+    int32_t ReInit();
+    int32_t PutCharacteristicProfile(const CharacteristicProfile& charProfile);
+    int32_t GetCharacteristicProfile(const std::string& deviceId, const std::string& serviceName,
+        const std::string& characteristicKey, CharacteristicProfile& charProfile);
+    int32_t GetAllCharacteristicProfile(std::vector<CharacteristicProfile>& staticCapabilityProfiles);
+
+private:
+    int32_t GenerateStaticInfoProfile(const CharacteristicProfile& staticCapabilityProfile,
+        std::unordered_map<std::string, CharacteristicProfile>& staticInfoProfiles);
+
+private:
+    std::mutex staticStoreMutex_;
+    std::shared_ptr<IKVAdapter> staticProfileStore_ = nullptr;
+};
+} // namespace DistributedDeviceProfile
+} // namespace OHOS
+#endif // OHOS_DP_STATIC_PROFILE_MANAGER_H
---
+++ services/core/src/persistenceadapter/kvadapter/switch_adapter.cpp
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "switch_adapter.h"
+
+#include "distributed_device_profile_errors.h"
+#include "distributed_device_profile_log.h"
+#include "kv_data_change_listener.h"
+#include "types.h"
+
+namespace OHOS {
+namespace DistributedDeviceProfile {
+IMPLEMENT_SINGLE_INSTANCE(SwitchAdapter);
+
+namespace {
+    const std::string TAG = "SwitchAdapter";
+}
+
+void SwitchAdapter::Init()
+{
+    HILOGI("called");
+    std::lock_guard<std::mutex> lock(switchAdapterMutex_);
+    observer_ = std::make_shared<KvDataChangeListener>();
+}
+
+void SwitchAdapter::Uninit() {}
+
+int32_t SwitchAdapter::PutSwitch(const std::string& appId, uint32_t value, uint16_t length)
+{
+    HILOGI("called");
+    if (appId.empty()) {
+        HILOGE("params are invalid");
+        return DP_INVALID_PARAMS;
+    }
+    DistributedKv::AppId appID;
+    appID.appId = appId;
+    const DistributedKv::SwitchData switchData = {value, length};
+
+    std::lock_guard<std::mutex> lock(switchAdapterMutex_);
+    DistributedKv::Status res = kvDataMgr_.PutSwitch(appID, switchData);
+    if (res != DistributedKv::Status::SUCCESS) {
+        HILOGE("PutSwitch kv to db failed, ret: %d", res);
+        return DP_PUT_KV_DB_FAIL;
+    }
+    return DP_SUCCESS;
+}
+
+int32_t SwitchAdapter::GetSwitch(const std::string& appId, const std::string& networkId, uint32_t& value)
+{
+    HILOGI("called");
+    if (appId.empty() || networkId.empty()) {
+        HILOGE("params are invalid");
+        return DP_INVALID_PARAMS;
+    }
+    DistributedKv::AppId appID;
+    appID.appId = appId;
+
+    std::lock_guard<std::mutex> lock(switchAdapterMutex_);
+    auto res = kvDataMgr_.GetSwitch(appID, networkId);
+    if (res.first != DistributedKv::Status::SUCCESS) {
+        HILOGE("kv from db failed, ret: %d", res.first);
+        return DP_PUT_KV_DB_FAIL;
+    }
+    value = res.second.value;
+    return DP_SUCCESS;
+}
+
+int32_t SwitchAdapter::SubscribeSwitchData(const std::string& appId)
+{
+    HILOGI("called");
+    if (appId.empty()) {
+        HILOGE("appid is empty");
+        return DP_INVALID_PARAMS;
+    }
+    DistributedKv::AppId appID;
+    appID.appId = appId;
+
+    std::lock_guard<std::mutex> lock(switchAdapterMutex_);
+    auto res = kvDataMgr_.SubscribeSwitchData(appID, observer_);
+    if (res != DistributedKv::Status::SUCCESS) {
+        HILOGE("SubscribeSwitchData failed, ret: %d", res);
+        return DP_SUBSCRIBE_FAILED;
+    }
+    return DP_SUCCESS;
+}
+
+int32_t SwitchAdapter::UnsubscribeSwitchData(const std::string& appId)
+{
+    HILOGI("called");
+    if (appId.empty()) {
+        HILOGE("appid is empty");
+        return DP_INVALID_PARAMS;
+    }
+
+    DistributedKv::AppId appID;
+    appID.appId = appId;
+    std::lock_guard<std::mutex> lock(switchAdapterMutex_);
+    auto res = kvDataMgr_.UnsubscribeSwitchData(appID, observer_);
+    if (res != DistributedKv::Status::SUCCESS) {
+        HILOGE("SubscribeSwitchData failed, ret: %d", res);
+        return DP_SUBSCRIBE_FAILED;
+    }
+    return DP_SUCCESS;
+}
+} // namespace DeviceProfile
+} // namespace OHOS
