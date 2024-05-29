cbb0ee2b17bfcde23e4730f322722c090c89c8f8
torrizo <lishenhao2@huawei.com>
2024-05-29 11:36:27 +0000

    add fuzz
    Signed-off-by: torrizo <lishenhao2@huawei.com>

--- services/core/test/fuzztest/profileutils_fuzzer/profileutils_fuzzer.h
+++ services/core/test/fuzztest/profileutils_fuzzer/profileutils_fuzzer.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,9 +13,9 @@
  * limitations under the License.
  */
 
-#ifndef SYNC_DEVICE_PROFILE_INNER_FUZZER_H
-#define SYNC_DEVICE_PROFILE_INNER_FUZZER_H
+#ifndef PROFILE_UTILS_FUZZER_H
+#define PROFILE_UTILS_FUZZER_H
 
-#define FUZZ_PROJECT_NAME "syncdeviceprofileInner_fuzzer"
+#define FUZZ_PROJECT_NAME "profileutils_fuzzer"
 
 #endif
