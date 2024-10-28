884ac5934409b85a8bdcce52a18b97ad2752521c
torrizo <lishenhao2@huawei.com>
2024-10-28 07:56:54 +0000

    update old/services/core/include/devicemanager/dp_device_manager.h.
    Signed-off-by: torrizo <lishenhao2@huawei.com>

--- old/services/core/include/devicemanager/dp_device_manager.h
+++ old/services/core/include/devicemanager/dp_device_manager.h
@@ -65,6 +65,9 @@ private:
 
 private:
     std::mutex deviceLock_;
+    std::mutex initcallbackLock_;
+    std::mutex stateCallLock_;
+    std::mutex devMgrLock_;
     std::shared_ptr<AppExecFwk::EventHandler> devMgrHandler_;
     std::shared_ptr<DistributedHardware::DeviceStateCallback> stateCallback_;
     std::shared_ptr<DistributedHardware::DmInitCallback> initCallback_;
